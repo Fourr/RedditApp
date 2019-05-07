8a1d206e02d4126a1157311deccf3e7ab539b4c6
Johnny Sheffer <johnnysheffer@gmail.com>
2019-05-06 20:10:55 -0400

    This is the initial Commit

---
+++ RedditApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditApp
+//
+//  Created by Johnny Sheffer on 5/6/19.
+//  Copyright © 2019 johnny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
